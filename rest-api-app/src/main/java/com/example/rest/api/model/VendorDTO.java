0f952ddea70da7a0cbc8bd8a5b8a03887e217cc9
nikoartz <nikitaartsiomchyk@gmail.com>
2021-06-11 19:57:41 +0200

    Spring documents generating

--- rest-api-app/src/main/java/com/example/rest/api/model/VendorDTO.java
+++ rest-api-app/src/main/java/com/example/rest/api/model/VendorDTO.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * @author martsiomchyk
  */
@@ -12,6 +15,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VendorDTO {
+    @NotBlank
+    @Size(min = 1, max = 255)
     private String name;
     private String vendorUrl;
 
--- rest-api-app/src/test/java/com/example/rest/api/controller/VendorControllerTest.java
+++ rest-api-app/src/test/java/com/example/rest/api/controller/VendorControllerTest.java
@@ -7,11 +7,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
 
 import java.util.Arrays;
 
@@ -22,15 +26,25 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
+
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest(controllers = {VendorController.class})
 public class VendorControllerTest {
 
@@ -66,22 +80,39 @@ public class VendorControllerTest {
 
         given(vendorService.getVendorById(anyLong())).willReturn(vendorDTO_1);
 
-        mockMvc.perform(get(VendorController.BASE_URL + "/1")
+        mockMvc.perform(get(VendorController.BASE_URL + "/{vendorId}", "1")
+                .param("isNew", "yes")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", equalTo(vendorDTO_1.getName())));
+                .andExpect(jsonPath("$.name", equalTo(vendorDTO_1.getName())))
+                .andDo(document("v1/vendor-get",
+                        pathParameters(
+                                parameterWithName("vendorId").description("Vendor ID.")
+                        ),
+                        requestParameters(
+                                parameterWithName("isNew").description("Is new vendor query parameter.")
+                        ),
+                        responseFields(
+                                fieldWithPath("name").description("Name of a vendor."),
+                                fieldWithPath("vendorUrl").description("Url of a vendor.")
+                        )));
     }
 
     @Test
     public void createNewVendor() throws Exception {
 
         given(vendorService.createNewVendor(vendorDTO_1)).willReturn(vendorDTO_1);
-
+        ConstrainedFields fields = new ConstrainedFields(VendorDTO.class);
         mockMvc.perform(post(VendorController.BASE_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(asJsonString(vendorDTO_1)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name", equalTo(vendorDTO_1.getName())));
+                .andExpect(jsonPath("$.name", equalTo(vendorDTO_1.getName())))
+                .andDo(document("v1/vendor-new",
+                        requestFields(
+                                fields.withPath("name").description("Name of a vendor request."),
+                                fields.withPath("vendorUrl").description("Url of a vendor request.")
+                        )));
     }
 
     @Test
@@ -114,5 +145,20 @@ public class VendorControllerTest {
 
         then(vendorService).should().deleteVendorById(anyLong());
 
+    }
+
+    private static class ConstrainedFields {
+
+        private final ConstraintDescriptions constraintDescriptions;
+
+        ConstrainedFields(Class<?> input) {
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+
+        private FieldDescriptor withPath(String path) {
+            return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+                    .collectionToDelimitedString(this.constraintDescriptions
+                            .descriptionsForProperty(path), ". ")));
+        }
     }
 }
